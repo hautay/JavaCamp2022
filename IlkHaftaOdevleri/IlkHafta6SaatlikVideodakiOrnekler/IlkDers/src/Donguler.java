53b64353fdc007db0abeb99951afbe121fa2beaa
Hakan Utku Ayçiçek <hautay@yandex.com>
2022-09-27 23:42:01 +0300

    İkinci Haftanın Ödevleri Eklendi

---
+++ IlkHaftaOdevleri/IlkHafta6SaatlikVideodakiOrnekler/IlkDers/src/Donguler.java
@@ -0,0 +1,29 @@
+
+public class Donguler {
+
+	public static void main(String[] args) {
+		
+		String urun = "Laptop";
+		for(int i=1;i<10;i++) {
+			System.out.println(urun + i);
+		}
+		
+		for(int i=0;i<=10;i+=2){
+			System.out.println(i);
+		}
+		
+		
+		int sayi = 10;
+		while(sayi<=100) {
+			System.out.println(sayi);
+			sayi = sayi + 10;
+		}
+		
+		int sayi2 = 11;
+		do {
+			System.out.println(sayi2);
+			sayi = sayi + 10;
+		}while(sayi2>100);
+	}
+
+}
